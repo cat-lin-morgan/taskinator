380a02b061b4671d93df242189c224cd5297f92a
cat - lin <plummorgan@gmail.com>
2020-08-26 19:36:33 -0700

    added form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+// var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    console.log(event);
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler); // <--callback function example
-
-
-
+formEl.addEventListener("submit", createTaskHandler); // <--callback function example
 
