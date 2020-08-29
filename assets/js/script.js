d51afc0c911fca579175b519570d95991934414c
cat - lin <plummorgan@gmail.com>
2020-08-28 17:13:16 -0700

    update drag over effects

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,6 +143,8 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
+
     }
 };
 var dropTaskHandler = function(event) {
@@ -158,8 +160,16 @@ var dropTaskHandler = function(event) {
     } else if (statusType === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
     }
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if(taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+}
 
 
 formEl.addEventListener("submit", taskFormHandler); // <--callback function example
@@ -171,4 +181,5 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
     
